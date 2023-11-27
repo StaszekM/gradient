4e2d1e93aed440363ba048b8c069650a231ed287
Julia Janiak <92268625+JuliaJ194@users.noreply.github.com>
2023-11-27 13:37:43 +0100

    GRA-18 add classes descriptions

--- src/baseline_models/GCN/gcn.py
+++ src/baseline_models/GCN/gcn.py
@@ -4,7 +4,27 @@ import torch
 
 
 class GCNModel(nn.Module):
+    """
+    Graph Convolutional Network (GCN) Model.
+    
+    Description:
+        This class defines a simple Graph Convolutional Network (GCN) model using PyTorch Geometric. The model consists of
+        two graph convolutional layers with ReLU activation functions.
+    Attributes:
+        conv1: The first GCN layer.
+        act1: ReLU activation after the first GCN layer.
+        conv2: The second GCN layer.
+        act2: ReLU activation after the second GCN layer.
+    """
     def __init__(self, in_dim: int, hidden_dim: int, out_dim: int):
+        """
+        Init GCNModel.
+
+        Args:
+            in_dim (int): Input feature dimension.
+            hidden_dim (int): Dimension of the hidden layer.
+            out_dim (int): Output dimension.
+        """
         super().__init__()
         self.conv1 = GCNConv(in_dim, hidden_dim)
         self.act1 = nn.ReLU()
@@ -17,6 +37,17 @@ class GCNModel(nn.Module):
         edge_index: torch.Tensor,
         edge_weight: torch.Tensor,
     ) -> torch.Tensor:
+        """
+        Forward method of the GCN model.
+
+        Args:
+            x (torch.Tensor): Input node features.
+            edge_index (torch.Tensor): Graph edge indices.
+            edge_weight (torch.Tensor): Edge weights.
+
+        Returns:
+            torch.Tensor: Output tensor after the forward pass.
+        """
         x = self.act1(self.conv1(x, edge_index, edge_weight))
         x = self.act2(self.conv2(x, edge_index, edge_weight))
         return x
--- src/baseline_models/classifier.py
+++ src/baseline_models/classifier.py
@@ -1,64 +1,79 @@
-from typing import Tuple
-import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, ConfusionMatrixDisplay
-import matplotlib
+from sklearn.metrics import ConfusionMatrixDisplay, roc_auc_score, accuracy_score, precision_score, recall_score, f1_score
+
 
 class CustomClassifier:
-    def __init__(self, data: pd.DataFrame, target_column: str, model=None):
-        self.data = data
-        self.target_column = target_column
+    """
+    A custom classifier that provides a simple interface for training and evaluating machine learning models.
+
+    """
+    def __init__(self, model=None):
+        """
+        Init CustomClassifier.
+
+        Args:
+            model: Scikit-learn compatible classification model (default: LogisticRegression)
+
+        """
         self.clf_model = model if model else LogisticRegression()
-        self.X = None
-        self.y = None
-
-    def _prepare_data(self):
-        self.X = self.data.loc[:, self.data.columns != self.target_column]
-        self.y = self.data.loc[:, self.data.columns == self.target_column]
-
-    def split(self, test_size: float = 0.2) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-        self._prepare_data()
-        return train_test_split(self.X, self.y, test_size=test_size, stratify=self.y)
 
     def train(self, X_train: np.ndarray, y_train: np.ndarray) -> None:
+        """
+        Trains the classifier on the given training data.
+
+        Args:
+            X_train: Input features for training.
+            y_train: Target labels for training.
+        """
         self.clf_model.fit(X_train, y_train)
 
     def predict(self, X_test: np.ndarray) -> np.ndarray:
+        """
+        Predicts the labels for the given test data.
+
+        Args:
+            X_test: Input features for testing.
+
+        Returns:
+            Predicted labels for the input test data.
+        """
         return self.clf_model.predict(X_test)
 
-    def results(self, y_test: np.ndarray, y_pred: np.ndarray):
+    def evaluate(self, X_test: np.ndarray, y_test: np.ndarray, y_pred: np.ndarray) -> dict:
+        """
+        Evaluates the classifier's performance using various metrics and displays results.
+
+        Parameters:
+            X_test: Input features for testing.
+            y_test: True labels for the test data.
+            y_pred: Predicted labels for the test data.
+        Returns:
+            Dictionary containing evaluation metrics (F1 score, accuracy, precision, recall, AUC score).
+        """
+        metrics = {}
+
+        f1 = f1_score(y_test, y_pred)
+        metrics['F1 Score'] = f1
+
+        accuracy = accuracy_score(y_test, y_pred)
+        metrics['Accuracy'] = accuracy
+
+        precision = precision_score(y_test, y_pred, average='weighted')
+        metrics['Precision'] = precision
+
+        recall = recall_score(y_test, y_pred, average='weighted')
+        metrics['Recall'] = recall
+
+        if hasattr(self.clf_model, "predict_proba"):
+            y_prob = self.clf_model.predict_proba(X_test)[:, 1]
+            auc_score = roc_auc_score(y_test, y_prob)
+            metrics['AUC Score'] = auc_score
+
         fig, ax = plt.subplots()
         ConfusionMatrixDisplay.from_predictions(y_test, y_pred, labels=self.clf_model.classes_, ax=ax)
         fig.suptitle("Confusion Matrix for the Baseline Classifier")
         plt.show()
-        print(classification_report(y_test, y_pred, output_dict=False))
 
-    def visualize_tsne(self, data: pd.DataFrame = None, n_iter: int = 500) -> None:
-        tsne = TSNE(n_iter=n_iter)
-        ts = tsne.fit_transform(self.X if data is None else data)
-
-        flattened_y = self.data[self.target_column].astype('category').cat.codes
-
-        cmap = matplotlib.colormaps.get_cmap('viridis')
-        colors_array = np.arange(0, np.unique(flattened_y).shape[0])
-        colors_array = colors_array / colors_array.max()
-        colors_array = cmap(colors_array)
-
-        fig, ax = plt.subplots()
-        sc = ax.scatter(ts[:, 0], ts[:, 1], cmap="viridis", c=flattened_y)
-        ax.set_xlabel('t-SNE Dimension 1')
-        ax.set_ylabel('t-SNE Dimension 2')
-        fig.colorbar(sc, ax=ax, label=self.target_column)
-
-        legend_elements = [Line2D([0], [0], marker='o', color='w', markerfacecolor=col, markersize=10,
-                                  label=f'Class {x}') for x, col in enumerate(colors_array)]
-
-        plt.legend(handles=legend_elements)
-        fig.suptitle('t-SNE 2D plot of data points')
-        fig.set_size_inches(10, 6)
-        plt.show()
+        return metrics
