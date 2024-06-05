84c9228e725f5a8096d21273e9888d0bd08ba461
Stanisław Markowski <stasiomimi@hotmail.com>
2024-06-05 14:05:43 +0200

    Fix feature importance for hexes airbnb

--- src/downstream_tasks/accidents_prediction/feature_importance.py
+++ src/downstream_tasks/accidents_prediction/feature_importance.py
@@ -10,19 +10,19 @@ import numpy as np
 def get_feature_importance():
     api = wandb.Api()
     model_artifact = api.artifact(
-        "gradient_pwr/accidents-downstream-task-v2/model_b3fs9h38_fold_1:v0"
+        "gradient_pwr/airbnb-downstream-task/model_mz2gb5xm_fold_0:v0"
     )  # the best model from the hex-only sweep on accidents task (or any other task)
     path_str = model_artifact.download()
 
-    with open(path_str + "/model_b3fs9h38_fold_1.pkl", "rb") as f:
+    with open(path_str + "/model_mz2gb5xm_fold_0.pkl", "rb") as f:
         model: LogisticRegression = cast(LogisticRegression, load(f))
 
     data_artifact = api.artifact(
-        "gradient_pwr/accidents-downstream-task-v2/tabular_data:v2"
+        "gradient_pwr/airbnb-downstream-task/tabular_data:v20"
     )  # data artifact from the same sweep
 
     data_path = data_artifact.download()
-    with open(data_path + "/tabular_data_25f95thh.pkl", "rb") as f:
+    with open(data_path + "/tabular_data_cij69bob.pkl", "rb") as f:
         data = load(f)
     train_X = pd.concat([x["X"] for _, x in data.items()])  # just for getting col names
 
